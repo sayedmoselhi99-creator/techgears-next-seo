3367f23a6efa9b99b4055fda4cec6e6f65fc7582
sayedmoselhi99-creator <sayedmoselhi99@gmail.com>
2025-12-07 22:14:27 +0200

    update sitemap

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -146,6 +146,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../app/api/auth/indexnow/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/auth/indexnow">> = Specific
+  const handler = {} as typeof import("../../app/api/auth/indexnow/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
